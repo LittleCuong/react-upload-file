f7e82765e71c1e1c3edcb0dfea97c6533f7b5412
Cuong Nguyen <littlecuong922@gmail.com>
2024-06-23 10:09:41 +0700

    Update Backend Auth

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,26 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend"
 
-defineBackend({
-  auth,
-  data,
-});
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ */
+const backend = defineBackend({})
+
+backend.addOutput({
+  auth: {
+    aws_region: "ap-southeast-1",
+    user_pool_id: "ap-southeast-1_PrB1LBC61",
+    user_pool_client_id: "1in55sf06h9beto16jk37offki",
+    identity_pool_id: "ap-southeast-1:23751e12-17a0-49c9-b9c6-90bf54ac5919",
+    username_attributes: ["email"],
+    standard_required_attributes: ["email"],
+    user_verification_types: ["email"],
+    unauthenticated_identities_enabled: true,
+    password_policy: {
+      min_length: 8,
+      require_lowercase: true,
+      require_uppercase: true,
+      require_numbers: true,
+      require_symbols: true,
+    }
+  }
+})
