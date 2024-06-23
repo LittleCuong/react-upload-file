99233f21982a737a62af72f02b9a95a65fb68820
Cuong Nguyen <littlecuong922@gmail.com>
2024-06-23 18:45:25 +0700

    Add storage access

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,4 +2,10 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'uploadFileCuongAWS',
+  access: (allow) => ({
+    'picture-submissions/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read'])
+    ],
+  })
 });
