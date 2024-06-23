809aa8cbd49d3efb5de0dbe72db982c115170a4d
Cuong Nguyen <littlecuong922@gmail.com>
2024-06-23 18:00:06 +0700

    Fix error

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,9 @@
 import { defineBackend } from "@aws-amplify/backend"
 import { data } from './data/resource';
-import { storage } from './storage/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-const backend = defineBackend({data, storage})
+const backend = defineBackend({data})
 
 backend.addOutput({
   auth: {
